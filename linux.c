974bcca3215c76125f23a8ddb3948d53996b953b
555isfaiz <1244274837@qq.com>
2022-09-01 16:35:19 +0200

    fix read error for linux

--- linux.c
+++ linux.c
@@ -106,6 +106,11 @@ void clipboard_monitor_loop()
         {
             int cb_len = 0;
             char *cb_buf = read_local_clipboard(&cb_len);
+            if (cb_len <= 0)
+            {
+                free(cb_buf);
+                continue;
+            }
             char send_buf[8192] = {0};
             int msg_len = gen_msg_clipboard_update(send_buf);
             memcpy(send_buf + msg_len, cb_buf, cb_len);
